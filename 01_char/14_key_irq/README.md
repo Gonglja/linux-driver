185dcb3558e8f76c765c2cdd998cbfd1ca20b9c9
Gonglja <glj0@outlook.com>
2022-07-18 20:38:35 +0800

    FEAT: 添加中断触发示例（目前有问题）

---
+++ 01_char/14_key_irq/README.md
@@ -0,0 +1,174 @@
+## 更新前
+
+~~目前插入、卸载驱动正常。设备树可正常识别出来~~
+设备树如下，通过 of_get_next_child 获取子节点
+```c
+/{
+    keys: keys {
+		compatible = "smart210,keys";
+		pinctrl-name = "default";
+		pinctrl-0 = <&pinctrl_keys>;
+		status="okay";
+		button1 {
+			gpio-pin = <&gph2 0 0>;
+		};		
+		
+		button2 {
+			gpio-pin = <&gph2 1 0>;
+		};		
+		
+		button3 {
+			gpio-pin = <&gph2 2 0>;
+		};		
+		
+		button4 {
+			gpio-pin = <&gph2 3 0>;
+		};		
+		
+		button5 {
+			gpio-pin = <&gph3 0 0>;
+		};		
+		
+		button6 {
+			gpio-pin = <&gph3 1 0>;
+		};		
+		
+		button7 {
+			gpio-pin = <&gph3 2 0>;
+		};		
+		
+		button8 {
+			gpio-pin = <&gph3 3 0>;
+		};
+	};
+};
+
+
+&pinctrl0 {
+    ...
+    pinctrl_keys: keysgrp {
+		samsung,pins = "gph2-0", "gph2-1", "gph2-2", "gph2-3", 
+					   "gph3-0", "gph3-1", "gph3-2", "gph3-3";
+		samsung,pin-function = <EXYNOS_PIN_FUNC_INPUT>;
+		samsung,pin-pud = <S3C64XX_PIN_PULL_UP>;
+		samsung,pin-drv = <EXYNOS4_PIN_DRV_LV1>;
+	};
+}
+
+```
+
+
+```shell
+# insmod  key.ko 
+[ 1286.690792][  T119] key_init
+[ 1286.691541][  T119] keys node has been found!
+[ 1286.691632][  T119] gpio-pin num[0] = 221 
+[ 1286.691699][  T119] gpio-pin num[1] = 222 
+[ 1286.691757][  T119] gpio-pin num[2] = 223 
+[ 1286.691816][  T119] gpio-pin num[3] = 224 
+[ 1286.692584][  T119] gpio-pin num[4] = 229 
+[ 1286.696743][  T119] gpio-pin num[5] = 230 
+[ 1286.705111][  T119] gpio-pin num[6] = 231 
+[ 1286.705201][  T119] gpio-pin num[7] = 232 
+[ 1286.709890][  T119] major:250
+# rmmod key
+[ 1288.260512][  T121] key_exit
+# 
+
+```
+
+
+
+---
+
+## 更新后
+
+通过 `./key_test -w ff `设置8个按键均可被捕获，
+此时通过`./key_test -r ` 就可以等待捕获，直到有按键被按下，打印出按键编号，也即是当button1 按下时，打印0x1，当button2按下时，打印0x2 ...
+
+| bits | buttonx |
+| ---- | ------- |
+| 0    | button1 |
+| 1    | button2 |
+| 2    | button3 |
+| 3    | button4 |
+| 4    | button5 |
+| 5    | button6 |
+| 6    | button7 |
+| 7    | button8 |
+
+具体日志看下面
+
+```shell
+# 
+# ls
+buzzer.ko       hello_drv.ko    key.ko          led.ko
+buzzer_test     hello_drv_test  key_test        led_test
+# insmod key.ko 
+[   49.052784][  T109] key: loading out-of-tree module taints kernel.
+[   49.053730][  T109] key_init
+[   49.054554][  T109] keys node has been found!
+[   49.054648][  T109] gpio-pin num[0] = 221 
+[   49.054718][  T109] gpio-pin num[1] = 222 
+[   49.054780][  T109] gpio-pin num[2] = 223 
+[   49.056577][  T109] gpio-pin num[3] = 224 
+[   49.062772][  T109] gpio-pin num[4] = 229 
+[   49.065408][  T109] gpio-pin num[5] = 230 
+[   49.072627][  T109] gpio-pin num[6] = 231 
+[   49.073227][  T109] gpio-pin num[7] = 232 
+[   49.077400][  T109] major:251
+# ./key_test -w ff
+open file /dev/key ok
+status:255
+[   66.365454][  T110] kernel recvdata size:1 
+[   66.365533][  T110] kernel recvdata size:1 buf:0xff
+# ./key_test -r 
+open file /dev/key ok
+[   79.272283][  T111] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x1
+# ./key_test -r 
+open file /dev/key ok
+[   82.443276][  T112] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x2
+# ./key_test -r 
+open file /dev/key ok
+[   83.986593][  T113] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x4
+# ./key_test -r 
+open file /dev/key ok
+[   85.502276][  T114] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x8
+# ./key_test -r 
+open file /dev/key ok
+[   87.175377][  T115] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x10
+# ./key_test -r 
+open file /dev/key ok
+[   88.528172][  T116] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x20
+# ./key_test -r 
+open file /dev/key ok
+[   89.975625][  T117] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x40
+# ./key_test -r 
+open file /dev/key ok
+[   92.662628][  T118] kernel senddata ok!
+APP read size: 0
+APP read buff: 0x80
+# rmmod key
+[  106.491140][  T119] key_exit
+# 
+# 
+
+```
+
+
+
+/sys/kernel/debug 为空，在文件系统中添加 `debugfs /sys/kernel/debug debugfs defaults 0 0`到`/etc/fstab` 重启就可以看到里面的内容了。
---
+++ 01_char/14_key_irq/key.c
@@ -0,0 +1,274 @@
+#include <linux/fs.h>
+#include <linux/module.h>
+#include <linux/uaccess.h>
+#include <asm/io.h>
+#include <linux/cdev.h>
+#include <linux/device.h>       /* struct class */
+#include <linux/of.h>           /* of_xx */
+#include <linux/of_address.h>   /* of_iomap */
+#include <linux/of_gpio.h>      /* of_gpio gpio */
+#include <linux/platform_device.h>/* platform */
+#include <linux/mutex.h>        /* mutex */
+#include <linux/timer.h>        /* timer_list/timer_setup/mod_timer */
+#include <linux/of_irq.h>       /* irq_of_parse_and_map */
+#include <linux/interrupt.h>    /* request_itq */
+
+
+#define KEY_NUM         8
+
+
+struct key_irq {
+    int                 gpio;       /* gpio num */
+    int                 irqnum;     /* irq num  */
+    unsigned char       val;        /* key value */
+    char                name[10];   /* name */
+    irqreturn_t         (*handler)(int, void *); /* interrupt handler */
+};
+
+struct key_dev{
+    dev_t               devid;      /* 设备号 */
+    struct cdev         cdev;       /* cdev  */
+    struct class        *class;     /* class */
+    struct device       *device;    /* device */
+    char                *name;      /* name */
+    int                 major;      /* 主设备号 */
+    int                 minor;      /* 次设备号 */
+    struct device_node  *nd;        /* 设备节点 */
+    int                 keys[KEY_NUM];    /* 8个key的gpio编号 */
+    struct mutex        mutex;      /* 互斥量 */
+    unsigned char       select[0];  /* 可选择8个按键，0x1 << i */
+    struct timer_list   timer;      /* timer */
+    unsigned long       timer_data; /* timer data */
+    struct key_irq      keyirq[KEY_NUM];     /* key irq */   
+    unsigned char       curkeynum;  /* current key number */
+};
+
+static struct key_dev keydev;
+
+
+static int key_open(struct inode *node, struct file *file) 
+{
+    file->private_data = &keydev;   /* 设置私有数据 */
+
+    /* 获取互斥锁，进入休眠状态的进程可以被信号打断 */
+	if(mutex_lock_interruptible(&keydev.mutex)) {
+        return -ERESTARTSYS;
+    }
+#if 0
+    mutex_lock(&keydev.mutex); /* 不能被信号打断 */
+#endif
+    return 0;
+}
+
+
+static int key_release(struct inode *node, struct file *file) 
+{
+    /* 释放互斥锁 */
+    mutex_unlock(&keydev.mutex);
+    return 0;
+}
+
+
+static ssize_t key_read(struct file *file, char __user *buf, size_t size, loff_t *offset) 
+{
+    int ret;
+    struct key_dev *dev = file->private_data;
+    char res[1] = {0};
+    // 先读key引脚状态，在将该状态cpoy to user
+    while(!res[0]) {
+        for(int i=0; i<8; ++i) {
+            if(dev->select[0] & (1 << i)) {
+                // 先读key引脚状态
+                res[0] |= ((gpio_get_value(dev->keys[i]) ? 0 : 1) << i);
+            }
+        }
+    }
+
+    ret = copy_to_user(buf, res, 1);
+    if( ret == 0 ) {
+        printk("kernel senddata ok!\r\n");
+    } else {
+        printk("kernel senddata failed!\r\n");
+    }
+    return 0;
+}
+
+
+static ssize_t key_write(struct file *file, const char __user *buf, size_t size, loff_t *offset) 
+{
+    struct key_dev *dev = file->private_data;
+    printk("kernel recvdata size:%u \r\n", size);
+    if(size == 1) {
+        int ret = copy_from_user(dev->select, buf, size);
+        if( ret == 0) {
+            printk("kernel recvdata size:%u buf:%#x\r\n", size, dev->select[0]);
+
+        } else {
+            printk("kernel recvdata failed!\r\n");
+        }
+    }
+    return 0;
+}
+
+
+
+static struct file_operations key_ops = {
+    .owner      = THIS_MODULE,
+    .open       = key_open,
+    .release    = key_release,
+    .read       = key_read,
+    .write      = key_write
+};
+
+
+static irqreturn_t key_handler(int irq, void *dev_id) 
+{
+    struct key_dev *dev = (struct key_dev *)dev_id;
+    dev->curkeynum = 0;
+    dev->timer_data = (volatile long)dev_id;
+    mod_timer(&dev->timer, jiffies + msecs_to_jiffies(10));
+    return IRQ_RETVAL(IRQ_HANDLED);
+}
+
+void timer_callback(struct timer_list *tl) 
+{
+    unsigned char num;
+    struct key_irq *key;
+    struct key_dev *dev = from_timer(dev, tl, timer);
+    
+    num = dev->curkeynum;
+    key = &dev->keyirq[num];
+    key->val = gpio_get_value(&dev->keys[num]);
+
+}
+
+static int keys_probe(struct platform_device *pdev) 
+{
+    int ret;
+    struct device_node *first_child_node=NULL;
+ 
+    /* 获取设备树中的属性数据 */
+    /* 1.获取设备节点：led */
+    keydev.nd = of_find_node_by_path("/keys");
+    if( keydev.nd == NULL ) {
+        printk("keys node can not found!\r\n");
+        return -EINVAL;
+    } else {
+        printk("keys node has been found!\r\n");
+    }
+
+    /* 2.获取led-pin 属性内容 */
+    for(int i=0; i<KEY_NUM; ++i) {
+        /* 获取下一个子节点，传入两个参数，第一个为父节点，第二个如果为NULL，则返回第一个子节点，如果为上一个子节点，则返回下一个子节点*/
+        first_child_node = of_get_next_child(keydev.nd, first_child_node);
+        keydev.keyirq[i].gpio = keydev.keys[i] = of_get_named_gpio(first_child_node, "gpio-pin", 0);
+        if(keydev.keys[i] < 0) {
+            printk("can't get gpio-pin.\r\n");
+            return -EINVAL;
+        } else {
+            printk("gpio-pin num[%d] = %d \r\n", i, keydev.keys[i]);
+        }
+
+        memset(keydev.keyirq[i].name, 0, sizeof(keydev.keyirq[i].name));
+        sprintf(keydev.keyirq[i].name, "KEY%d", i);
+        gpio_request(keydev.keyirq[i].gpio, keydev.keyirq[i].name);
+
+        gpio_direction_input(keydev.keys[i]);
+
+        /*从设备树中获取按键 IO 对应的中断号 */
+        keydev.keyirq[i].irqnum = irq_of_parse_and_map(keydev.nd, i);
+        printk("keydev.keyirq[i].irqnum %#x\r\n", keydev.keyirq[i].irqnum);
+    }
+    
+    /* 3. 设置pin脚为输入模式 */
+
+    
+    /* req interrupt */
+    keydev.keyirq[0].handler = key_handler;
+    keydev.keyirq[0].val     = 0x1;
+
+    for(int i=0; i<KEY_NUM; ++i) {
+        ret = request_irq(keydev.keyirq[i].irqnum, keydev.keyirq[i].handler,
+                          IRQF_TRIGGER_RISING | IRQF_TRIGGER_FALLING,
+                          keydev.keyirq[i].name, &keydev);
+        if(ret<0) {
+            printk("irq %d request failed!\r\n", keydev.keyirq[i].irqnum);
+        }
+    }
+
+    keydev.name = "key";
+    /* 如果主设备号不为空，则注册设备*/
+    if(keydev.major) {
+        keydev.devid = MKDEV(keydev.major, 0);
+        register_chrdev_region(keydev.devid, 1, keydev.name);
+    } else {
+        /* 否则申请设备号 */
+        alloc_chrdev_region(&keydev.devid, 0, 1, keydev.name);
+        keydev.major = MAJOR(keydev.devid); /* 申请的主设备号  */
+        keydev.minor = MINOR(keydev.devid); /* 申请的次设备号 */
+    }
+    /* cdev结构初始化，cdev 表示一个字符设备 */
+    cdev_init(&keydev.cdev, &key_ops);
+
+    /* cdev 添加设备*/
+    cdev_add(&keydev.cdev, keydev.devid, 1);
+
+    /* 可以通过 cat /proc/devices 查看 */
+    printk("major:%d\r\n", keydev.major);
+
+    /* class 创建类 */
+    keydev.class = class_create(THIS_MODULE, keydev.name);
+
+    /* device 创建设备 */
+    keydev.device = device_create(keydev.class, NULL, keydev.devid, NULL, keydev.name);
+
+    /* timer */
+    timer_setup(&keydev.timer, timer_callback, 0U);
+    mod_timer(&keydev.timer, jiffies + msecs_to_jiffies(200));
+
+    return 0;
+}
+
+static int keys_remove(struct platform_device *pdev) 
+{
+    cdev_del(&keydev.cdev);
+    unregister_chrdev_region(keydev.major, 1);
+    device_destroy(keydev.class, keydev.devid);
+    class_destroy(keydev.class);
+    return 0;
+}
+
+static const struct of_device_id of_keys_match[] = {
+    { .compatible = "s5pv210,keys", },
+    {},
+};
+MODULE_DEVICE_TABLE(of, of_keys_match);
+
+static struct platform_driver keys_driver = {
+    .probe      = keys_probe,
+    .remove     = keys_remove,
+    .driver     = {
+        .name   = "keys",
+        .of_match_table = of_keys_match,
+    },
+};
+
+
+static int __init keys_init(void) 
+{
+    printk("key_init\r\n");
+    /* 初始化互斥锁 */
+    mutex_init(&keydev.mutex);
+    return platform_driver_register(&keys_driver);
+}
+
+
+static void __exit keys_exit(void) 
+{
+    printk("key_exit\r\n");
+    platform_driver_unregister(&keys_driver);
+}
+
+MODULE_LICENSE("GPL");
+module_init(keys_init);
+module_exit(keys_exit);
